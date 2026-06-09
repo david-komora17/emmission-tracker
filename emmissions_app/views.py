ee18fce717799977c0c53ac16484e4c6aa1a4df7
Gracesvg <134380789+Gracesvg@users.noreply.github.com>
2026-06-10 02:00:46 +0300

    Create views for the user versus admin feedback loop.

--- emmissions_app/views.py
+++ emmissions_app/views.py
@@ -4,9 +4,11 @@ from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 from rest_framework import status
 
-from .models import Activity
-from .permissions import HasPremiumPlan  # The permission class you wrote in Week 1
+from .models import Activity, SystemComplaint
+from .serializers import SystemComplaintSerializers
+from .permissions import isAdminUserRole  # The permission class you wrote in Week 1
 from .services.ai_coach import generate_eco_recommendations
+
 
 class PremiumAICoachView(APIView):
     """
@@ -45,3 +47,30 @@ class PremiumAICoachView(APIView):
             },
             status=status.HTTP_200_OK
         )
+    
+class ComplaintFunnelView (APIView):
+    """
+    Dual-role view.
+    - Authenticated Users can POST complaints via the widget.
+    - Admin accounts can GET a list of all system complaints.
+    """
+    def get_permissions(self):
+        if self.request.method == 'GET':
+            return [isAdminUserRole()]
+        return [IsAuthenticated()]
+
+    def get(self, request):
+        complaints = SystemComplaint.objects.all()
+        serializer = SystemComplaintSerializer(complaints, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    
+    def post(self, request):
+        serializer = SystemComplaintSerializer(data=request.data)
+        if serializer.is_valid():
+            # Automatically bind the logged in user to their complaint row.
+            serializer.save(user, request.user)
+            return Response(
+                {"message": "Complaint is received and processed. Expect feedback shortly!"},
+                status=status.HTTP_201_CREATED
+            )
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
