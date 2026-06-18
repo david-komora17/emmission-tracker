9db1c1522441d80f4d95de55d2a05d77589cff5b
Gracesvg <134380789+Gracesvg@users.noreply.github.com>
2026-06-18 10:33:49 +0300

    Ensure to change the syntax in wsgi.py file to be able to deploy the web app in render.

--- emmissions_project/wsgi.py
+++ emmissions_project/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'emmissions_project.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'emmission-tracker.settings')
 
 application = get_wsgi_application()
